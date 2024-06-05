e9a2e770ee7edc207879bc387c46e81d74ef7b6b
Rozalia Ravnachka <rozalia.ravnachka2002@gmail.com>
2024-06-05 17:57:42 +0300

    add navbar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,18 +1,21 @@
-import Navbar from "../components/dashboard/navbar/navbar";
-import Sidebar from "../components/dashboard/sidebar/sidebar";
+import Navbar from "../ui/dashboard/navbar/navbar"
+import Sidebar from "../ui/dashboard/sidebar/sidebar"
+import styles from "../ui/dashboard/dashboard.module.css"
+import Footer from "../ui/dashboard/footer/footer"
 
-const layout = ({ children }) => {
+const Layout = ({children}) => {
   return (
-    <div>
-      <div>
-        <Sidebar />
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar/>
       </div>
-      <div>
-        <Navbar />
+      <div className={styles.content}>
+        <Navbar/>
         {children}
+        <Footer/>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default layout;
+export default Layout
